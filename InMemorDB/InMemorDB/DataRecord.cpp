2a8853b2de9b21b238423f225c3ed08c095b8bda
Ssergio0 <sergio7salguero@gmail.com>
2023-11-16 08:02:07 -0600

    Implementación las funciones de la clase DataRecord

--- InMemorDB/InMemorDB/DataRecord.cpp
+++ InMemorDB/InMemorDB/DataRecord.cpp
@@ -1,1 +1,24 @@
 #include "DataRecord.h"
+#include <sstream>
+#include <iomanip>
+
+DataRecord::DataRecord(const std::string& key, const std::vector<std::string>& fields)
+    : primaryKey(key), dataFields(fields) {
+    generateHashKey();
+}
+
+std::string DataRecord::getPrimaryKey()const {
+    return primaryKey;
+}
+
+std::string DataRecord::getHashKey()const {
+    return hashKey;
+}
+
+void DataRecord::generateHashKey() {
+
+}
+
+const std::vector<std::string>& DataRecord::getDataFields()const {
+    return dataFields;
+}
