a2a9655744d70be7fd2df529fc239627ca991a0d
Ssergio0 <sergio7salguero@gmail.com>
2023-11-17 09:42:08 -0600

    Primera prueba de implementación hash con librerias de c++

--- InMemorDB/InMemorDB/HashFunction.cpp
+++ InMemorDB/InMemorDB/HashFunction.cpp
@@ -1,1 +1,8 @@
 #include "HashFunction.h"
+
+unsigned long HashFunction::hash(const std::string& key) {
+	unsigned long hash = 5381;
+	for (char c : key)
+		hash = ((hash << 5) + hash) + c;
+	return hash;
+}
--- InMemorDB/InMemorDB/HashFunction.h
+++ InMemorDB/InMemorDB/HashFunction.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <string>
 class HashFunction
 {
+public:
+	static unsigned long hash(const std::string& key);
 };
 
