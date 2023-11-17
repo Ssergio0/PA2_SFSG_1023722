7a97df32d2a5df97d437c277fc251bb71543f8d1
Ssergio0 <sergio7salguero@gmail.com>
2023-11-17 09:37:46 -0600

    Implementación de la lista enlazada

--- InMemorDB/InMemorDB/LinkedList.cpp
+++ InMemorDB/InMemorDB/LinkedList.cpp
@@ -1,4 +1,40 @@
 #include "LinkedList.h"
 
+LinkedList::LinkedList() :head(nullptr), tail(nullptr) {}
 
+LinkedList::~LinkedList() {
+	DataItem* current = head;
+	while (current != nullptr)
+	{
+		DataItem* next = current->next;
+		delete current;
+		current = next;
+	}
+}
+void LinkedList::insertSorted(DataItem* newItem) {
+	if (!head || newItem->keyPrimary < head->keyPrimary)
+	{
+		if (head) head->prev = newItem;
+		newItem->next = head;
+		head = newItem;
+		return;
+	}
+	
+	DataItem* current = head;
+	while (current->next && current->next->keyPrimary < newItem->keyPrimary) 
+		current = current->next;
 
+	newItem->next = current->next;
+	newItem->prev = current;
+	if (current->next)
+		current->next->prev = newItem;
+	tail = newItem;
+	current->next = newItem;
+}
+
+DataItem* LinkedList::search(const std::string& key) {
+	DataItem* current = head;
+	while (current && current->keyPrimary != key)
+		current = current->next;
+	return current;
+}
--- InMemorDB/InMemorDB/LinkedList.h
+++ InMemorDB/InMemorDB/LinkedList.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "DataItem.h"
 #include "DataRecord.h"
 #include <memory>
 #include <unordered_map>
@@ -7,6 +8,8 @@
 class LinkedList
 {
 private:
+	DataItem* head;
+	DataItem* tail;
 	struct Node
 	{
 		std::unique_ptr<DataRecord> data;
@@ -15,5 +18,12 @@ private:
 	};
 	/*Node(std::unique_ptr<DataRecord> dataValue, Node* prevNode, std::unique_ptr<Node> nextNode)
 		: data(std::move(dataValue)), prev(prevNode), next(std::move(nextNode)) {}*/
+
+public:
+	LinkedList();
+	~LinkedList();
+
+	void insertSorted(DataItem* newItem);
+	DataItem* search(const std::string& key);
 };
 
